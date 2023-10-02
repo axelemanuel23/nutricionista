bf687dec4dc6b5e895c187153ee6f552403fa519
Lex A <axelemanuelpavon@gmail.com>
2023-10-02 16:25:28 -0300

    Generando la estructura dedl proyecto

---
+++ nutricionista/src/main/java/eq39/nutricionista/Nutricionista.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package eq39.nutricionista;
+
+/**
+ *
+ * @author axele
+ */
+public class Nutricionista {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
